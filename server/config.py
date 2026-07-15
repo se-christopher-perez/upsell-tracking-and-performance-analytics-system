c2981edd41e32cb1840994a2af54a6b8d91f42bc
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-07-15 13:49:13 -0400

    Begining Installations and Configs: pipenv install flask flask-sqlalchemy flask-migrate flask-bcrypt flask-restful flask-cors and npm create vite@latest client -- --template react

---
+++ server/config.py
@@ -0,0 +1,18 @@
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_restful import Api
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = "captsone_upsell_project_secret"
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
+api = Api(app)
+CORS(app, supports_credentials=True)
