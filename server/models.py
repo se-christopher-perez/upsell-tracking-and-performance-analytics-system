79b940838a390615f176c0e5a9f5969a948c4b8a
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-07-15 15:07:15 -0400

    Serial rules where added to each of the models along with adding the bcrypt password_hash, setter and authenticate

--- server/models.py
+++ server/models.py
@@ -7,15 +7,30 @@ class User(db.Model, SerializerMixin):
 
     __tablename__ = "users"
 
+    serialize_rules = ["-bills.user", "-_password_hash"]
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     _password_hash = db.Column(db.String, nullable=False)
 
     bills = db.relationship("Bill", back_populates="user")
 
+    @property
+    def password_hash(self):
+        raise AttributeError("password_hash is not readable.")
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        self._password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password)
+
 class Bill(db.Model, SerializerMixin):
 
     __tablename__ = "bills"
+
+    serialize_rules = ["-user.bills", "-items.bill"]
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
@@ -30,6 +45,8 @@ class Item(db.Model, SerializerMixin):
 
     __tablename__ = "items"
 
+    serialize_rules = ["-bill.items", "-interactions.item"]
+
     id = db.Column(db.Integer, primary_key=True)
     bill_id = db.Column(db.Integer, db.ForeignKey('bills.id'), nullable=False)
     item_name = db.Column(db.String)
@@ -43,6 +60,8 @@ class Item(db.Model, SerializerMixin):
 class Interaction(db.Model, SerializerMixin):
 
     __tablename__ = "interactions"
+
+    serialize_rules = ["-item.interactions", "-terms.interaction"]
 
     id = db.Column(db.Integer, primary_key=True)
     item_id = db.Column(db.Integer, db.ForeignKey('items.id'), nullable=False)
@@ -60,8 +79,10 @@ class Term(db.Model, SerializerMixin):
 
     __tablename__ = "terms"
 
+    serialize_rules = ["-interaction.terms"]
+
     id = db.Column(db.Integer, primary_key=True)
     interaction_id = db.Column(db.Integer, db.ForeignKey('interactions.id'), nullable=False)
-    term = db.Column(db.String, unique=True)
+    term = db.Column(db.String)
 
     interaction = db.relationship("Interaction", back_populates="terms")
