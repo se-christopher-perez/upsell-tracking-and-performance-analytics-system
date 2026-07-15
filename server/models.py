707207b0eb787efc7b8d91559fd560afee4de193
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-07-15 14:22:19 -0400

    Created models for Users, Bills, Items, Interactions, and Terms and connected them through relationships

---
+++ server/models.py
@@ -0,0 +1,67 @@
+
+from config import db, bcrypt
+from sqlalchemy.orm import validates
+from sqlalchemy_serializer import SerializerMixin
+
+class User(db.Model, SerializerMixin):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)
+
+    bills = db.relationship("Bill", back_populates="user")
+
+class Bill(db.Model, SerializerMixin):
+
+    __tablename__ = "bills"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    total = db.Column(db.Float)
+    tip = db.Column(db.Float)
+    created_at = db.Column(db.Date)
+
+    user = db.relationship("User", back_populates="bills")
+    items = db.relationship("Item", back_populates="bill")
+
+class Item(db.Model, SerializerMixin):
+
+    __tablename__ = "items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    bill_id = db.Column(db.Integer, db.ForeignKey('bills.id'), nullable=False)
+    item_name = db.Column(db.String)
+    category = db.Column(db.String)
+    price = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
+
+    bill = db.relationship("Bill", back_populates="items")
+    interactions = db.relationship("Interaction", back_populates="item")
+
+class Interaction(db.Model, SerializerMixin):
+
+    __tablename__ = "interactions"
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'), nullable=False)
+    approach = db.Column(db.String)
+    upsell = db.Column(db.Boolean)
+    feedback = db.Column(db.String)
+    customer_gender = db.Column(db.String)
+    customer_carded = db.Column(db.Boolean)
+    customer_repeat = db.Column(db.Boolean)
+
+    item = db.relationship("Item", back_populates="interactions")
+    terms = db.relationship("Term", back_populates="interaction")
+
+class Term(db.Model, SerializerMixin):
+
+    __tablename__ = "terms"
+
+    id = db.Column(db.Integer, primary_key=True)
+    interaction_id = db.Column(db.Integer, db.ForeignKey('interactions.id'), nullable=False)
+    term = db.Column(db.String, unique=True)
+
+    interaction = db.relationship("Interaction", back_populates="terms")
