06240e399c525de4b1d5fa9a56a9a9e3c6badb02
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-07-17 15:40:46 -0400

    created a Contect Provider for check session

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,45 @@
+
+import { createContext, useContext, useState, useEffect } from "react"
+
+const AuthContext = createContext()
+
+export function AuthProvider({ chrildren }) {
+
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetch("http://localhost:5556/check_session", {
+            
+            headers: {"Content-Type": "application/json"},
+            credentials: "include"
+
+        })
+        .then((r) => {
+
+            r.ok ? r.json() : null
+
+        })
+        .then((data) => setUser(data))
+        .catch((err) => console.log(err))
+        .finally(() => setLoading(false))
+
+    }, [])
+
+    const value = { user, setUser, loading }
+
+    return (
+
+        <AuthContext.Provider value={value}>
+            {chrildren}
+        </AuthContext.Provider>
+
+    )
+
+}
+
+export function useAuth() {
+
+    return useContext(AuthContext)
+
+}
